014e0c37ca1672feef440a3121383af9b8f4b214
NathiyaUmapathi <nathiyaumapathi082922@gmail.com>
2026-06-09 23:00:05 +0530

    upgraded hooks.js file

--- support/hooks.js
+++ support/hooks.js
@@ -1,15 +1,17 @@
-const { Before, After } = require('@cucumber/cucumber');
+const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 
+setDefaultTimeout(60000);
+
 Before(async function () {
-    this.browser = await chromium.launch({ headless: false });
+
+    const isCI = process.env.CI === 'true';
+
+    this.browser = await chromium.launch({
+        headless: isCI ? true : false
+    });
+
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
-});
-
-After(async function () {
-    if (this.browser) {
-        await this.browser.close();
-    }
 });
 
